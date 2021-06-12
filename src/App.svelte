9149a49ce8b33727be07c3cbee76e767d0ae4157
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2021-06-12 19:09:01 +0100

    Start expanding section

--- src/App.svelte
+++ src/App.svelte
@@ -5,14 +5,37 @@
 <main>
 	<h1>1-4 mora nouns</h1>
 
-	<h2>Heat</h2>
+	<h2>1-mora nouns</h2>
+
+	<ul>
+		<li>
+			<p>70% are 頭高, being primarily Chinese in origin, and tend to represent ideas or concepts.</p>
+		</li>
+		<li>
+			<p>30% are 平板, and tend to be everyday nouns and objects.</p>
+		</li>
+	</ul>
+
+	<h3>Body parts</h3>
+
+	<div class="pitchesContainer">
+		<StyledPitch title="目が痛い" word="^め|!がい^た|い"/>
+		<StyledPitch title="手が痛い" word="^て|!がい^た|い"/>
+		<StyledPitch title="歯が痛い" word="^は|!がい^た|い"/>
+	</div>
+	
+	<div class="pitchesContainer">
+		<StyledPitch title="胃が痛い" word="い^!がいた|い"/>
+	</div>
+
+	<h3>Heat</h3>
 
 	<div class="pitchesContainer">
 		<StyledPitch title="火が強い" word="^ひ|!がつ^よ|い"/>
 		<StyledPitch title="日が出る" word="ひ^!がで|る"/>
 	</div>
 
-	<h2>Life</h2>
+	<h3>Life</h3>
 
 	<div class="pitchesContainer">
 		<StyledPitch title="木が高い" word="^き!|がた^か|い"/>
